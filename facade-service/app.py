1ff92ac2a132251c84376f2edc4aa866ab02e94a
kryachka <34610965+kryachka@users.noreply.github.com>
2021-04-06 00:03:30 +0300

    lab4

--- facade-service/app.py
+++ facade-service/app.py
@@ -1,11 +1,19 @@
+import random
 import uuid
 import requests
 from flask import Flask, request
 
 app = Flask(__name__)
 message_url = "http://localhost:9001"
-logging_url = "http://localhost:9000"
+logging_url = "http://localhost:5703"
 
+
+def get_rand_logging_client():
+    return random.choice(["http://localhost:5703", "http://localhost:5704", "http://localhost:5705"])
+
+
+rand = get_rand_logging_client()
+print(rand)
 
 @app.route('/facade-service', methods=['GET', 'POST'])
 def facadeService():
@@ -17,7 +25,7 @@ def facadeService():
 
 def get_request():
     print('send GET-like request to logging service')
-    get_logging_response = requests.get(f'{logging_url}/logging-service')
+    get_logging_response = requests.get(f'{rand}/logging-service')
     print('Received from logging service: ', get_logging_response)
     m_response = requests.get(f'{message_url}/messages')
     print('received from messages:', m_response.content)
@@ -28,11 +36,12 @@ def get_request():
 def post_request():
     print('POST request to logging service')
     l_request = {"uuid": str(uuid.uuid4()), "msg": request.json.get('msg')}
-    l_response = requests.post(f'{logging_url}/logging-service', json=l_request)
+    l_response = requests.post(f'{rand}/logging-service', json=l_request)
     status = l_response.status_code
     print('Received status from logging service:', status)
     stat = app.response_class(status=status)
     return stat
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9002, debug=True)
--- logging_service/s2.py
+++ logging_service/s2.py
@@ -1,20 +1,18 @@
 from flask import request, Flask
 import hazelcast
-
 app = Flask(__name__)
 
 dictionary = {}
 client = hazelcast.HazelcastClient(
-    cluster_name="dev",
-    cluster_members=[
-        "127.0.0.1:5703",
-        "127.0.0.1:5704",
-        "127.0.0.1:5705"
-    ],
-    lifecycle_listeners=[lambda state: print("Lifecycle:", state), ]
-)
-
-
+        cluster_name="dev",
+        cluster_members=[
+            "127.0.0.1:5703",
+            "127.0.0.1:5704",
+            "127.0.0.1:5705",
+        ],
+        lifecycle_listeners=[lambda state: print("Lifecycle:", state), ]
+    )
+distributed_map=client.get_map("map")
 @app.route('/logging-service', methods=['GET', 'POST'])
 def loggingService():
     if request.method == 'GET':
@@ -23,18 +21,17 @@ def loggingService():
         return post_request()
 
 
-distributed_map = client.get_map("map")
-
-
 def post_request():
     print(f'Received request: {request}..............')
     uuid = request.uuid
     msg = request.msg
     distributed_map.put(uuid, msg)
-    # dictionary.update({request.json["uuid"]: request.json["msg"]})
+    #dictionary.update({request.json["uuid"]: request.json["msg"]})
     print(f'saved .............')
     return app.response_class(status=200)
 
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5703, debug=True)
+    app.run(host='0.0.0.0', port=5704, debug=True)
+
